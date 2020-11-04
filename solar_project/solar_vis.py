6ef5725aa09c1890b070dc6e09603ccff3ecb77e
sarseniy <senokosov.ao@phystech.edu>
2020-11-04 22:46:41 +0300

    Drawing updated

--- solar_project/solar_vis.py
+++ solar_project/solar_vis.py
@@ -74,17 +74,16 @@ class Drawer:
     def update(self, figures, ui):
         self.screen.fill((0, 0, 0))
         for figure in figures:
-            figure.draw_on(self.screen)
+            figure.draw_on(self.screen, figure)
         
         ui.blit()
         ui.update()
         pg.display.update()
 
 
-class DrawableObject:
+class DrawableObject():
     def __init__(self, obj):
         self.obj = obj
 
-    def draw_on(self, surface):
-        object_ = self.obj
+    def draw_on(self, surface, object_):
         circle(surface, object_.color, (scale_x(object_.x), scale_y(object_.y)), object_.R)
